65319e67f5045683c39930273fae17f784f577be
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-04 11:06:22 +0300

    Update

--- app/DoubleLinkedList.py
+++ app/DoubleLinkedList.py
@@ -1,17 +1,29 @@
-class Node(object):
+'''This is Double-linked list module.'''
+
+class Node():
+	'''
+	This is double-linked list's node.
+	'''
 	prev = 0
 	next = 0
-	data = 0
 
+	def __init__(self, value):
+		self.data = value
 
-class DoubleLinkedList(object):
+	def __str__(self):
+		return str(self.data)
+
+	def get_prev(self):
+		'''Returns previous node.'''
+		return self.prev
+
+	def get_next(self):
+		'''Returns next node.'''
+		return self.next
+
+class DoubleLinkedList():
 	'''
-		Double-linked list.
-		
-		add(value) - adds new value to the end of the list
-		insert(index, value) - adds new value to the list after node with given index
-
-
+	Double-linked list.
 	'''
 
 	head = 0
@@ -19,42 +31,26 @@ class DoubleLinkedList(object):
 
 	def __insert_after_node(self, current, element):
 		'''Inserts given node (element) before  'current' node.'''
-		if (current == 0):
-			print('Error: null index!!!')
-		if (current.next != 0):
-			element.next = current.next
-			element.prev = current
-			current.next.prev = element
-			current.next = element
+		if current == 0:
+			if (self.tail == 0):
+				self.head = element
+				self.tail = self.head
+			else:
+				print('__insert_after_node: Null index.')
 		else:
-			element.next = 0
-			element.prev = current
-			current.next = element
-	def insert(self, index, value):
-		'''Inserts value by index'''
-		i = self.head
-		for _ in range(index):
-			i = i.next
-
-		tmp_node = Node()
-		tmp_node.data = value
-		self.__insert_after_node(i, tmp_node)
-
-	def add(self, value):
-		'''Adds new value at the end of the list.'''
-		if (self.tail == 0):
-			self.head = Node()
-			self.tail = self.head
-			self.head.data = value
-		else:
-			self.tail.next = Node()
-			self.tail.next.data = value
-			self.tail.next.prev = self.tail
-			self.tail = self.tail.next
+			if current.next != 0:
+				element.next = current.next
+				element.prev = current
+				current.next.prev = element
+				current.next = element
+			else:
+				element.next = 0
+				element.prev = current
+				current.next = element
 
 	def __str__(self):
 		'''Printing DoubleLinkedList.'''
-		if (self.head != 0):
+		if self.head != 0:
 			result = str(self.head.data)
 		i = self.head.next
 		while i != 0:
@@ -63,28 +59,17 @@ class DoubleLinkedList(object):
 
 		return result
 
-	def __get_node(self, index):
-		'''Returns node by index.'''
-		i = self.head
-		for _ in range(index):
-			i = i.next
-		return i
-
-	def get(self, index):
-		'''Returns data from node by index.'''
-		return self.__get_node(index).data
-
 	def __remove_by_node(self, node):
 		'''Removes given node'''
-		if (node.next == 0 and node.prev == 0):
+		if node.next == 0 and node.prev == 0:
 			del node
 			self.head = 0
 			self.tail = 0
-		elif (node.next == 0):
+		elif node.next == 0:
 			node.prev.next = 0
 			self.tail = node.prev
 			del node
-		elif (node.prev == 0):
+		elif node.prev == 0:
 			node.next.prev = 0
 			self.head = node.next
 			del node
@@ -92,6 +77,31 @@ class DoubleLinkedList(object):
 			node.prev.next = node.next
 			node.next.prev = node.prev
 			del node
+
+	def __get_node(self, index):
+		'''Returns node by index.'''
+		i = self.head
+		for _ in range(index):
+			i = i.next
+		return i
+
+	def insert(self, index, value):
+		'''Inserts value by index'''
+		i = self.head
+		for _ in range(index):
+			i = i.next
+
+		tmp_node = Node(value)
+		self.__insert_after_node(i, tmp_node)
+
+	def push(self, value):
+		'''adds new element to the end.'''
+		tmp = Node(value)
+		self.__insert_after_node(self.tail, tmp)
+
+	def get(self, index):
+		'''Returns data from node by index.'''
+		return self.__get_node(index).data
 
 	def pop(self, index):
 		'''Removes node by index and returned it.'''
@@ -108,6 +118,44 @@ class DoubleLinkedList(object):
 		print('tail = {}'.format(self.tail))
 		print('data:')
 		i = self.head
-		while (i != 0):
+		while i != 0:
 			print('[\n{}\nprev: {}\nnext:{}\ndata: {}\n]\n'.format(i, i.prev, i.next, i.data))
 			i = i.next
+
+	def unshift(self, value):
+		'''Adds new element to the beggining of the list.'''
+		tmp = Node(value)
+		self.__insert_after_node(self.head, tmp)
+
+	def shift(self):
+		'''Removes element from the beginning.'''
+		__remove_by_node(self.head)
+
+	def len(self):
+		'''Returns length of the list.'''
+		i = self.head
+		result = 0
+		while i != 0:
+			result += 1
+			i = i.next
+
+		return result
+
+	def delete(self, elem):
+		'''Removes element from the list.'''
+		self.__remove_by_node(elem)
+
+	def contains(self, elem):
+		'''Checks if element is in list.'''
+		i = self.head
+		while i != elem:
+			i.next
+		return True if i == elem else False
+
+	def first(self):
+		'''Returns first element.'''
+		return self.head
+
+	def last(self):
+		'''Returns last element.'''
+		return self.tail
