e3158bb2afcab4991177c0f2f7379cedceca4060
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-13 20:07:50 +0300

    2nd h/w deleted, gunicorn configured

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def application(env, start_resp):
-	start_resp('200', [('Content-Type', 'text/plain')])
-	return ['<b>Hello, WSGI!</b>'.encode('utf-8')]
+import datetime
+import json
+
+def application(environ, start_response):
+	status = '200 OK'
+	headers = [('Content-Type', 'application/json')]
+
+	#print(environ)
+
+	body = {'time': str(datetime.datetime.now().time())[:8], 'url': environ['wsgi.url_scheme']+'://'+environ['HTTP_HOST']}
+	start_response(status, headers)
+
+	return [ json.dumps(body).encode('utf-8') ]
