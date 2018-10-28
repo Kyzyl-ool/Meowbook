96195f5fbd670a9ae38d7dbc01b764800901fef1
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-28 04:44:37 +0300

    creating new chat

--- app/model.py
+++ app/model.py
@@ -15,3 +15,14 @@ def get_chats_list():
 	return query_all("""
 		SELECT * FROM "Chat";
 	""")
+
+def add_new_chat(chat_id, is_group_chat, topic, last_message):
+	execute("""
+		INSERT INTO "Chat" VALUES (%(chat_id)s, %(is_group_chat)s, %(topic)s, %(last_message)s);
+	""",
+	chat_id = int(chat_id),
+	is_group_chat = 't' if is_group_chat else 'f',
+	topic=str(topic),
+	last_message = int(last_message))
+	commit()
+	rollback()
