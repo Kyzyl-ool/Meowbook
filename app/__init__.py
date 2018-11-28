08562affacbfcb8287307735cadf33a95662e673
Kyzyl-ool <kyzyloolk@mail.ru>
2018-11-28 21:48:32 +0300

    Auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_jsonrpc import JSONRPC
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+jsonrpc = JSONRPC(app, '/')
 
 from .views import *
--- app/model.py
+++ app/model.py
@@ -16,12 +16,33 @@ def get_chats_list():
 		SELECT * FROM "Chat";
 	""")
 
-def add_new_chat(chat_id, is_group_chat, topic, last_message):
-	execute("""
-		INSERT INTO "Chat" VALUES (%(chat_id)s, %(is_group_chat)s, %(topic)s, %(last_message)s);
+def get_members_list(user_id):
+	return query_all("""
+		SELECT topic FROM "Chat"
+		JOIN "Member" ON "Chat".chat_id = "Member".chat_id
+		AND user_id = %(user_id)s;
 	""",
-	chat_id = int(chat_id),
-	is_group_chat = 't' if is_group_chat else 'f',
+	user_id = user_id)
+
+def add_new_chat(is_group_chat, topic, last_message):
+	execute("""
+		INSERT INTO "Chat" (is_group_chat, topic, last_message) VALUES (%(is_group_chat)s, %(topic)s, %(last_message)s);
+	""",
+	is_group_chat = str(is_group_chat),
 	topic=str(topic),
 	last_message = int(last_message))
 	commit()
+
+def add_new_message(chat_id, user_id, content, sent):
+	execute("""
+		INSERT INTO "Message" (chat_id, user_id, content, sent) VALUES (%(chat_id)s, %(user_id)s, %(content)s, %(sent)s);
+		""",
+		chat_id = int(chat_id), user_id = int(user_id), content = str(content), sent = sent
+	)
+	commit()
+
+def get_messages(chat_id, user_id):
+	return query_all("""
+		SELECT content FROM "Message"
+		WHERE chat_id = %(chat_id)s AND user_id = %(user_id)s;
+		""", chat_id = int(chat_id), user_id = int(user_id))
